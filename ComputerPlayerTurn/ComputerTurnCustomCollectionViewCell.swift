add7a622821849f8972030988e3324abf2131926
Dawid Zimoch <dawid.jerzy.zimoch@gmail.com>
2022-05-26 16:57:12 +0200

    jjj

--- ComputerPlayerTurn/ComputerTurnCustomCollectionViewCell.swift
+++ ComputerPlayerTurn/ComputerTurnCustomCollectionViewCell.swift
@@ -8,10 +8,36 @@
 import UIKit
 
 class ComputerTurnCustomCollectionViewCell: UICollectionViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    
+    let identifier = "ComputerTurnCustomCollectionViewCell"
+    var state: fieldState = .free
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
     }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func actualizeState(newState: fieldState) {
+        state = newState
+        switch state {
+            case .free:
+                contentView.backgroundColor = .systemTeal
+            case .occupied:
+                contentView.backgroundColor = .systemGray
+            case .nearToOccupied:
+                contentView.backgroundColor = .orange
+            case .hit:
+                contentView.backgroundColor = .yellow
+            case .hitOccupied:
+                contentView.backgroundColor = .red
+        }
+    }
+    
 
 }
+
+
+

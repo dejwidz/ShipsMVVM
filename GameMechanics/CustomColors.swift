159cb9dc9f3697e90caec7b93e138dad1133aea1
Dawid Zimoch <dawid.jerzy.zimoch@gmail.com>
2023-02-08 20:25:39 +0100

    before deleting storybord

---
+++ GameMechanics/CustomColors.swift
@@ -0,0 +1,19 @@
+//
+//  CustomColors.swift
+//  ShipsMVVM
+//
+//  Created by Dawid Zimoch on 08/02/2023.
+//
+
+import Foundation
+import UIKit
+
+struct CustomColors {
+    
+    static var backColor = UIColor(named: "backColor")
+    static var fontColor = UIColor(named: "fontColor")
+    static var freeColor = UIColor(named: "freeColor")
+    static var hitColor = UIColor(named: "hitColor")
+    static var hitOccupiedColor = UIColor(named: "hitoccupiedColor")
+    static var tealAndGrayblue = UIColor(named: "teal&grayblue")
+}
--- ShipsMVVM/ViewController.swift
+++ ShipsMVVM/ViewController.swift
@@ -7,11 +7,48 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
     
-    @IBAction private func newGameButtonTapped(_ sender: UIButton) {
+    //    @IBAction private func newGameButtonTapped(_ sender: UIButton) {
+    //        let vc = CreateGameViewController()
+    //        navigationController?.pushViewController(vc, animated: true)
+    //    }
+    
+    private let w = UIScreen.main.bounds.width
+    private let h = UIScreen.main.bounds.height
+    
+    let newGameButton: UIButton = {
+        let button = UIButton()
+        let w = UIScreen.main.bounds.width
+        let h = UIScreen.main.bounds.height
+        button.setTitle("Start New Game", for: .normal)
+        button.setTitleColor(CustomColors.fontColor, for: .normal)
+        button.backgroundColor = CustomColors.tealAndGrayblue
+        button.layer.cornerRadius = h * 0.025
+        button.addTarget(self, action: #selector(newGameButtonTapped(_:)), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    @objc func newGameButtonTapped(_ sender: UIButton) {
         let vc = CreateGameViewController()
         navigationController?.pushViewController(vc, animated: true)
     }
     
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = CustomColors.backColor
+        setupInterface()
+    }
+    
+    func setupInterface() {
+        view.addSubview(newGameButton)
+        NSLayoutConstraint.activate([
+            newGameButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            newGameButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            newGameButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.8),
+            newGameButton.heightAnchor.constraint(equalToConstant: h * 0.05)
+        ])
+    }
 }

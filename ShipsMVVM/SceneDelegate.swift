f8f0f6f79843f4b8ce9260d47741d35729bf12f4
Dawid Zimoch <dawid.jerzy.zimoch@gmail.com>
2023-04-16 00:52:09 +0200

    storyboard removed

--- ShipsMVVM/SceneDelegate.swift
+++ ShipsMVVM/SceneDelegate.swift
@@ -12,6 +12,17 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else {
+            return
+        }
+        
+        let window = UIWindow(windowScene: windowScene)
+        
+        let homeViewController = ViewController()
+        let navigationController = UINavigationController(rootViewController: homeViewController)
+        navigationController.navigationBar.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height * 0.1)
+        window.rootViewController = navigationController
+        window.makeKeyAndVisible()
+        self.window = window
     }
 }
